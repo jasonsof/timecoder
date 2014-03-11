aa23d72eebdc93506860a7983f908f053cdc34a6
jason <jsofokleous@googlemail.com>
2014-03-11 16:08:18 +0000

    added basic implementation of timecode_in_seconds method

--- lib/timecoder.rb
+++ lib/timecoder.rb
@@ -1,5 +1,14 @@
 require "timecoder/version"
 
 module Timecoder
-  # Your code goes here...
+  def self.timecode_in_seconds(timecode)
+    timecode_split = timecode.split(":")
+    raise if timecode_split.length != 3
+
+    hours = timecode.split(":")[0].to_i
+    minutes = timecode.split(":")[1].to_i
+    seconds = timecode.split(":")[2].to_i
+
+    seconds + (minutes*60) + (hours*60*60)
+  end
 end
--- spec/timecoder_spec.rb
+++ spec/timecoder_spec.rb
@@ -1,1 +1,30 @@
 require 'spec_helper'
+
+describe Timecoder do
+  describe ".timecode_in_seconds" do
+    context "the input is a valid timecode" do
+      it "converts a timecode string into the equivalent time in seconds" do
+        expect(Timecoder.timecode_in_seconds("00:00:28")).to eq 28
+        expect(Timecoder.timecode_in_seconds("01:00:28")).to eq 3600 + 28
+        expect(Timecoder.timecode_in_seconds("00:10:00")).to eq 600
+        expect(Timecoder.timecode_in_seconds("02:10:01")).to eq 7200 + 600 + 1
+      end
+
+      it "returns an integer" do
+        input = "00:00:28"
+
+        expect(Timecoder.timecode_in_seconds(input)).to be_a(Fixnum)
+      end
+    end
+
+    context "the input is not a valid timecode" do
+      it "raises an exception" do
+        input = "34567"
+
+        expect {
+          Timecoder.timecode_in_seconds(input)
+        }.to raise_error
+      end
+    end
+  end
+end
