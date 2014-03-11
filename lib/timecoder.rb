57950ad79f7c831340452a0830ef737d6ea7ee6e
jason <jsofokleous@googlemail.com>
2014-03-11 17:06:44 +0000

    added first implementation of seconds_to_timecode method

--- lib/timecoder.rb
+++ lib/timecoder.rb
@@ -11,4 +11,25 @@ module Timecoder
 
     seconds + (minutes*60) + (hours*60*60)
   end
+
+  def self.seconds_to_timecode(total_seconds)
+    raise if !total_seconds.class.eql?(Fixnum)
+
+    if total_seconds < 60
+      "00:00:#{total_seconds.to_s.rjust(2, '0')}"
+    elsif total_seconds < 60*60
+      minutes = total_seconds / 60
+      seconds = total_seconds - (minutes*60)
+      "00:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    else
+      hours = (total_seconds / 60) / 60
+      if (total_seconds - (hours*60*60)) > 60
+        minutes = (total_seconds - (hours*60*60)) / 60
+      else
+        minutes = 0
+      end
+      seconds = total_seconds - (minutes*60) - (hours*60*60)
+      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    end
+  end
 end
--- spec/timecoder_spec.rb
+++ spec/timecoder_spec.rb
@@ -8,6 +8,8 @@ describe Timecoder do
         expect(Timecoder.timecode_in_seconds("01:00:28")).to eq 3600 + 28
         expect(Timecoder.timecode_in_seconds("00:10:00")).to eq 600
         expect(Timecoder.timecode_in_seconds("02:10:01")).to eq 7200 + 600 + 1
+        expect(Timecoder.timecode_in_seconds("00:00:00")).to eq 0
+        expect(Timecoder.timecode_in_seconds("00:01:00")).to eq 60
       end
 
       it "returns an integer" do
@@ -23,6 +25,36 @@ describe Timecoder do
 
         expect {
           Timecoder.timecode_in_seconds(input)
+        }.to raise_error
+      end
+    end
+  end
+
+  describe ".seconds_to_timecode" do
+    context "the input is valid" do
+      it "converts time in seconds to a formatted timecode" do
+        expect(Timecoder.seconds_to_timecode(28)).to eq "00:00:28"
+        expect(Timecoder.seconds_to_timecode(3600 + 28)).to eq "01:00:28"
+        expect(Timecoder.seconds_to_timecode(600)).to eq "00:10:00"
+        expect(Timecoder.seconds_to_timecode(7200 + 600 + 1)).to eq "02:10:01"
+        expect(Timecoder.seconds_to_timecode(0)).to eq "00:00:00"
+        expect(Timecoder.seconds_to_timecode(60)).to eq "00:01:00"
+        expect(Timecoder.seconds_to_timecode(87654)).to eq "00:01:00"
+      end
+
+      it "returns a string" do
+        input = 28
+
+        expect(Timecoder.seconds_to_timecode(input)).to be_a(String)
+      end
+    end
+
+    context "the input is invalid" do
+      it "raises an exception" do
+        input = "28"
+
+        expect {
+          Timecoder.seconds_to_timecode(input)
         }.to raise_error
       end
     end
