9a7c0fd2646f2066bb7bbe87ba7957046dd148d7
jason <jsofokleous@googlemail.com>
2014-03-12 10:04:31 +0000

    update method name in tests to reflect change in the class

--- spec/timecoder_spec.rb
+++ spec/timecoder_spec.rb
@@ -4,18 +4,18 @@ describe Timecoder do
   describe ".timecode_in_seconds" do
     context "the input is a valid timecode" do
       it "converts a timecode string into the equivalent time in seconds" do
-        expect(Timecoder.timecode_in_seconds("00:00:28")).to eq 28
-        expect(Timecoder.timecode_in_seconds("01:00:28")).to eq 3600 + 28
-        expect(Timecoder.timecode_in_seconds("00:10:00")).to eq 600
-        expect(Timecoder.timecode_in_seconds("02:10:01")).to eq 7200 + 600 + 1
-        expect(Timecoder.timecode_in_seconds("00:00:00")).to eq 0
-        expect(Timecoder.timecode_in_seconds("00:01:00")).to eq 60
+        expect(Timecoder.timecode_to_seconds("00:00:28")).to eq 28
+        expect(Timecoder.timecode_to_seconds("01:00:28")).to eq 3600 + 28
+        expect(Timecoder.timecode_to_seconds("00:10:00")).to eq 600
+        expect(Timecoder.timecode_to_seconds("02:10:01")).to eq 7200 + 600 + 1
+        expect(Timecoder.timecode_to_seconds("00:00:00")).to eq 0
+        expect(Timecoder.timecode_to_seconds("00:01:00")).to eq 60
       end
 
       it "returns an integer" do
         input = "00:00:28"
 
-        expect(Timecoder.timecode_in_seconds(input)).to be_a(Fixnum)
+        expect(Timecoder.timecode_to_seconds(input)).to be_a(Fixnum)
       end
     end
 
@@ -24,7 +24,7 @@ describe Timecoder do
         input = "34567"
 
         expect {
-          Timecoder.timecode_in_seconds(input)
+          Timecoder.timecode_to_seconds(input)
         }.to raise_error
       end
     end
