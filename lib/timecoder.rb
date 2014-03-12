ac546b6708521d4dabe308e3438f8017a6eb1d5c
jason <jsofokleous@googlemail.com>
2014-03-12 11:00:18 +0000

    refactored for readablility

--- lib/timecoder.rb
+++ lib/timecoder.rb
@@ -1,5 +1,8 @@
 require "timecoder/version"
 
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 60*60
+
 module Timecoder
   def self.timecode_to_seconds(timecode)
     timecode_split = timecode.split(":")
@@ -9,27 +12,48 @@ module Timecoder
     minutes = timecode.split(":")[1].to_i
     seconds = timecode.split(":")[2].to_i
 
-    seconds + (minutes*60) + (hours*60*60)
+    seconds + minutes_to_seconds(minutes) + hours_to_seconds(hours)
   end
 
   def self.seconds_to_timecode(total_seconds)
     raise if !total_seconds.class.eql?(Fixnum)
 
-    if total_seconds < 60
-      "00:00:#{total_seconds.to_s.rjust(2, '0')}"
-    elsif total_seconds < 60*60
-      minutes = total_seconds / 60
-      seconds = total_seconds - (minutes*60)
-      "00:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    if total_seconds < SECONDS_IN_MINUTE
+      timecode_str(0, 0, total_seconds)
+    elsif total_seconds < SECONDS_IN_HOUR
+      minutes = seconds_to_minutes(total_seconds)
+      seconds = total_seconds - minutes_to_seconds(minutes)
+
+      timecode_str(0, minutes, seconds)
     else
-      hours = (total_seconds / 60) / 60
-      if (total_seconds - (hours*60*60)) > 60
-        minutes = (total_seconds - (hours*60*60)) / 60
-      else
-        minutes = 0
-      end
-      seconds = total_seconds - (minutes*60) - (hours*60*60)
-      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+      hours = seconds_to_hours(total_seconds)
+      total_seconds -= hours_to_seconds(hours)
+      minutes = seconds_to_minutes(total_seconds)
+      total_seconds -= minutes_to_seconds(minutes)
+      seconds = total_seconds
+      
+      timecode_str(hours, minutes, seconds)
     end
   end
+
+  private
+    def self.timecode_str(hours, minutes, seconds)
+      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    end
+
+    def self.minutes_to_seconds(minutes)
+      minutes * SECONDS_IN_MINUTE
+    end
+
+    def self.hours_to_seconds(hours)
+      hours * SECONDS_IN_HOUR
+    end
+
+    def self.seconds_to_minutes(seconds)
+      seconds / SECONDS_IN_MINUTE
+    end
+
+    def self.seconds_to_hours(seconds)
+      seconds / SECONDS_IN_HOUR
+    end
 end
